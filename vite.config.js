fc24dee3a5542a1ed095c60278470cc794404b16
Francisco3rd <169160777+Franciscooo3@users.noreply.github.com>
2025-05-24 01:00:34 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfoliooo/',   // Add this line, e.g. '/react_portfolio/'
   server: {
     port: 8001,
   },
